7e24daf2549d897c9b6126b614acb6eeb85defcd
yen_xuan <snake96274@gapp.nthu.edu.tw>
2021-06-26 16:53:38 +0800

    modify fixing...

--- components/ProfilePage/ProfileContent/AccountContent.js
+++ components/ProfilePage/ProfileContent/AccountContent.js
@@ -13,6 +13,13 @@ class AccountContent extends React.Component {
       email: '',
       name: '',
       password: '',
+      confirmPassword: '',
+      newPwdValid: false,
+      pwdValid: false,
+      valueName: '',
+      valueEmail: '',
+      input: {},
+      msg: {},
       pwdShow: false,
     };
 
@@ -21,21 +28,23 @@ class AccountContent extends React.Component {
       this.setState({
         account: userId.account, 
         name: userId.name, 
-        email: userId.email
+        email: userId.email,
+        password: userId.password,
     })
     }).catch(err => {
         console.error('Error while getUser', err);
     });
     this.handlePwdExpand = this.handlePwdExpand.bind(this);
+    this.pwdConfirm = this.pwdConfirm.bind(this);
 
     this.handleNameChange = this.handleNameChange.bind(this);
     this.handleEmailChange = this.handleEmailChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePwdSubmit = this.handlePwdSubmit.bind(this);
   }
   
   render() {
     let color = 'red';
-    let subcolor = 'blue';
     let data = qs.stringify({
       'account': this.state.account,
       'email': this.state.email,
@@ -47,7 +56,7 @@ class AccountContent extends React.Component {
     return (
       <>
       {/* header */}
-      <form onSubmit={this.handleSubmit}>
+      
       <h1 className="font-bold pl-3 pr-3 pb-3 text-xl">Profile & Account</h1>
       <hr></hr>
       {/* Account Block */}
@@ -71,8 +80,8 @@ class AccountContent extends React.Component {
           <div className=' my-2.5 container flex-col flex items-center bg-white py-2'>
             <div className='container ring-2 ring-gray-200 rounded-lg p-3 px-4 my-2 flex-row flex items-center cursor-default bg-white'>
               <label className="ml-6 font-medium overflow-hidden mr-2 w-60">New UserName</label>
-              <input className='text-center sm:mr-10 mx-3 sm:w-60 w-32 bg-white border-gray-200 border-b-2 p-1 outline-none focus:outline-none hover:border-red-200 focus:border-red-500 cursor-auto focus:placeholder-transparent' placeholder='Enter your username'  
-              nameChange={this.handleNameChange} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+              <input type="text" className='text-center sm:mr-10 mx-3 sm:w-60 w-32 bg-white border-gray-200 border-b-2 p-1 outline-none focus:outline-none hover:border-red-200 focus:border-red-500 cursor-auto focus:placeholder-transparent' placeholder='Enter your username'  
+              value={this.state.valueName}  onChange={this.handleNameChange}
           ></input>
             </div>
          </div>
@@ -90,19 +99,21 @@ class AccountContent extends React.Component {
             <div className='container ring-2 ring-gray-200 rounded-lg p-3 px-4 my-2 flex-row flex items-center cursor-default bg-white'>
               <label className="ml-6 font-medium overflow-hidden mr-2 w-60">New Email</label>
               <input className='text-center sm:mr-10 mx-3 sm:w-60 w-32 bg-white border-gray-200 border-b-2 p-1 outline-none focus:outline-none hover:border-red-200 focus:border-red-500 cursor-auto focus:placeholder-transparent' 
-            emailChange={this.handleEmailChange} placeholder='Enter your email' onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+              value={this.state.valueEmail} onChange={this.handleEmailChange} placeholder='Enter your email' 
           ></input>
             </div>
          </div>
         </div>
 
         {/* Password Part */}
+        
         <div className='container shadow flex-col rounded-lg p-4 my-3 flex items-center cursor-default bg-white max-w-xl w-auto'>
           <div className="container flex-row flex items-center">
             <div className={` ml-5 h-4 w-0.5 bg-${color}-500 ring-2 ring-${color}-500`}></div>
             <span className='ml-5 font-semibold overflow-hidden'>Password</span>
             <div className='flex-grow' />
-            <button  className='text-center rounded-lg border-3 focus:outline-none outline-none border-black-700 bg-gray-300 text-gray-600 p-2 font-semibold hover:bg-gray-600 hover:text-white' onClick={this.handlePwdExpand}>
+            <button  className='text-center rounded-lg border-3 focus:outline-none outline-none border-black-700 bg-gray-300 text-gray-600 p-2 font-semibold hover:bg-gray-600 hover:text-white' 
+            onClick={this.handlePwdExpand}>
             {this.state.pwdShow ? "Cancel" : "Change Password"}</button>
           </div>
            { this.state.pwdShow &&
@@ -110,7 +121,7 @@ class AccountContent extends React.Component {
             {
               <div className='my-2.5 container ring-2 ring-gray-200 rounded-lg p-3 px-4 flex-row flex items-center cursor-default bg-white'>
                 <label className="ml-6 font-medium overflow-hidden mr-2 w-60" for="curpwd">Current Password</label>
-                <input className='text-center sm:mr-10 mx-3 sm:w-60 w-32 bg-white border-gray-200 border-b-2 p-1 outline-none focus:outline-none hover:border-red-200 focus:border-red-500 cursor-auto focus:placeholder-transparent' 
+                <input type="password" name="curPwd" className='text-center sm:mr-10 mx-3 sm:w-60 w-32 bg-white border-gray-200 border-b-2 p-1 outline-none focus:outline-none hover:border-red-200 focus:border-red-500 cursor-auto focus:placeholder-transparent' 
                 placeholder='Current Password' onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
                 ></input>              
               </div>
@@ -118,23 +129,27 @@ class AccountContent extends React.Component {
             {
               <div className='my-2.5 container ring-2 ring-gray-200 rounded-lg p-3 px-4 flex-row flex items-center cursor-default bg-white'>
                 <label className="ml-6 font-medium overflow-hidden mr-2 w-60 " for="newpwd">New Password</label>
-                <input className='text-center sm:mr-10 mx-3 sm:w-60 w-32 bg-white border-gray-200 border-b-2 p-1 outline-none focus:outline-none hover:border-red-200 focus:border-red-500 cursor-auto focus:placeholder-transparent' 
-                placeholder='New Password' onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+                <input type="password" name="newPwd1" className='text-center sm:mr-10 mx-3 sm:w-60 w-32 bg-white border-gray-200 border-b-2 p-1 outline-none focus:outline-none hover:border-red-200 focus:border-red-500 cursor-auto focus:placeholder-transparent' 
+                placeholder='New Password' onChange={this.pwdConfirm} required value={this.state.input.newPwd1} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
                 ></input>              
               </div>
             }
             {
               <div className='my-2.5 container ring-2 ring-gray-200 rounded-lg p-3 px-4 flex-row flex items-center cursor-default bg-white'>
                 <label className="ml-6 font-medium overflow-hidden mr-2 w-60" for="newpwd">Confirm New Password</label>
-                <input className='text-center sm:mr-10 mx-3 sm:w-60 w-32 bg-white border-gray-200 border-b-2 p-1 outline-none focus:outline-none hover:border-red-200 focus:border-red-500 cursor-auto focus:placeholder-transparent' 
-                placeholder='New Password' onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
+                <input type="password" name="newPwd2" className='text-center sm:mr-10 mx-3 sm:w-60 w-32 bg-white border-gray-200 border-b-2 p-1 outline-none focus:outline-none hover:border-red-200 focus:border-red-500 cursor-auto focus:placeholder-transparent' 
+                placeholder='New Password' onChange={this.pwdConfirm} required value={this.state.input.newPwd2} onKeyPress={(e) => { e.key === 'Enter' && e.preventDefault(); }}
                 ></input>              
               </div>
             }
             {
+            
               <div className="mr-auto ml-3">
+              <div className="text-red-500 font-bold">{this.state.msg.newPwd1}</div>
+              <div>{this.state.msg.newPwd2}</div>
               <span className='cursor-pointer text-blue-700 hover:underline sm:text-base text-xs block'>Forgot Password?</span>
-              <button type='submit' className='ring-2 ring-green-600 bg-green-200 hover:bg-green-600 text-green-800 hover:text-white rounded-lg shadow-md pl-2 pr-2 pt-1 pb-1 focus:outline-none my-3' onClick={this.handleSubmit}>
+              <button className='ring-2 ring-green-600 bg-green-200 hover:bg-green-600 text-green-800 hover:text-white rounded-lg shadow-md pl-2 pr-2 pt-1 pb-1 focus:outline-none my-3' 
+              onClick={this.handlePwdSubmit}>
                 <span>Save</span>
               </button>
               </div>
@@ -142,9 +157,11 @@ class AccountContent extends React.Component {
         </div>
         }
         </div>
+       
 
         {/* Account Page Save or Discard */}
-        <button type='submit' className='ring-2 ring-green-600 bg-green-200 hover:bg-green-600 text-green-800 hover:text-white rounded-lg shadow-md p-2 focus:outline-none my-3' onClick={this.handleSubmit}>
+        <button type='submit' className='ring-2 ring-green-600 bg-green-200 hover:bg-green-600 text-green-800 hover:text-white rounded-lg shadow-md p-2 focus:outline-none my-3' 
+        onClick={this.handleSubmit}>
           <span>Save</span>
         </button>
         <Link href='/profile/account'>
@@ -154,37 +171,76 @@ class AccountContent extends React.Component {
             </a>
           </button> 
         </Link>
-      </form>
       </>
     );
   }
 
+  // Password Part
   handlePwdExpand () {
     this.setState({ pwdShow: !this.state.pwdShow, });
   }
+  pwdConfirm(e){
+    var inputPwd = this.state.input;
+    inputPwd[e.target.name] = e.target.value;
+    this.setState({
+      inputPwd
+    })
+  }
+  validation(){
+    var msg={};
+    if(this.state.input["newPwd1"] !== this.state.input["newPwd2"]){
+      msg["newPwd1"] = "!New Password Are Not Matching!";
+      this.setState({newPwdValid: false});
+    }
+    else{
+      console.log('new Password equal');
+      this.setState({newPwdValid: true});
+    }
+    this.setState({
+      msg: msg
+    })
+  }
+  handlePwdSubmit(e){
+    e.preventDefault();
+    if(this.validation())
+    {
+      var input = {};
+      input["newPwd1"] = "";
+      input["newPwd2"] = "";
+    }    
+    var msg={};
+    if("" === userId.password)
+      this.setState({pwdValid: true});
+    else{
+      this.setState({pwdValid: false});
+      msg["curPwd"] = "!Current Password Are Wrong!";
+      this.setState({msg: msg});
+    }
 
-  handleNameChange(value){
-    this.setState({name: value,});
+    if(this.state.newPwdValid && this.state.pwdValid ){
+      console.log('Pwd ok')
+    }
+  }
+  
+
+  // Basic Info Part
+  handleNameChange(event) {
+    this.setState({valueName: event.target.value});
   }
 
-  handleAccountChange(event){
-    this.accEdited(event.target.value);
+  handleEmailChange(event) {
+    this.setState({ valueEmail: event.target.value});
   }
-  handleAccEdit(value){
-    this.setState({account: value});
-  }
-  handleEmailChange(value){
-    this.setState({ email: value,});
 
-  }
   handleSubmit(event) {
-    if(this.state.name == '' || this.state.account == '' || this.state.email == '')
+    if(this.state.valueName == '' && this.state.valueEmail == '')
       alert('You should enter some text');
     else {
+      console.log('submit');
       let data = qs.stringify({
         'account': `${this.state.account}`,
-        'email': `${this.state.email}`,
-        'name': `${this.state.name}`,
+        'email': `${this.state.valueEmail}`,
+        'name': `${this.state.valueName}`,
         'avatar_url': '',
         'password': `${this.state.password}`,
       })
